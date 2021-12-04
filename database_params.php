fc706c3e1f17b17ad851720e1b22efb8087847db
VictorCcorimanya <raulccorimanyaalfaro@gmail.com>
2021-12-03 22:39:01 -0500

    [FIX] db connection params

--- database_params.php
+++ database_params.php
@@ -1,6 +1,6 @@
 <?php
-  $host = 'mysql:unix_socket=/cloudsql/php-projects-333723:southamerica-west1:tasks-db';
-  $user = 'root';
-  $password = 'emA6b4j01LspPwkL';
+  $host = 'localhost';
+  $user = 'tasksdev';
+  $password = 'tasksdbserver2021';
   $database = 'tasks';
 ?>
