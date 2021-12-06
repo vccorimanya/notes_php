30a48b391cc315cbbf601655d0608230454a4915
dccorimanya <datatheyvit123@gmail.com>
2021-12-05 21:59:14 -0500

    [FIX] htmlentities function xd

--- signup.php
+++ signup.php
@@ -52,13 +52,12 @@
 
     function mysql_entities_fix_string($connection, $string)
     {
-        return htmlentties(mysql_fix_string($connection, $string));
+        return htmlentities(mysql_fix_string($connection, $string));
     }
-
+  
     function mysql_fix_string($connection, $string)
     {
-        //if (get_magic_quotes_gpc())
-        //    $string = stripslashes($string);
+        //if (get_magic_quotes_gpc()) $string = stripslashes($string);
         return $connection->real_escape_string($string);
     }
 
